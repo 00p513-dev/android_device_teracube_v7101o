c2c8961af8a52de76bec4a0829069ff539217270
Varun Date <date.varun123@gmail.com>
2020-05-26 03:15:43 +1000

    bismuth: overlay-resurrection: Maintainer string

--- device.mk
+++ device.mk
@@ -63,7 +63,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-resurrection
 
 # Overlays -- replace official
 PRODUCT_PACKAGES += \
