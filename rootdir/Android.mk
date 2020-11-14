3245e5a14c72662664c62012afc5b45fac649cc3
TheMalachite <eliasgheeraert@gmail.com>
2020-11-14 04:13:32 +0100

    bismuth: Bind mount fstab.enableswap instead

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,14 +6,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.ago.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.ago.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mt6771.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
