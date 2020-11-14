b7febc375890cd22128b002ab7ea84c6d013f0eb
TheMalachite <eliasgheeraert@gmail.com>
2020-11-14 04:11:29 +0100

    bismuth: properties: Move all system props to product

--- properties.mk
+++ properties.mk
@@ -5,18 +5,12 @@
 #
 
 # Audio
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.camera.sound.forced=0 \
     ro.audio.silent=0
 
 # Display (Graphics, SurfaceFlinger, Video)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.opengles.version=196610 \
-    ro.zygote.preload.enable=0 \
-    qemu.hw.mainkeys=0 \
-    sys.ipo.disable=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     debug.composition.type=gpu \
     debug.cpurend.vsync=false \
     debug.egl.profiler=1 \
@@ -29,9 +23,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
     debug.sf.early_phase_offset_ns=1500000 \
     debug.sf.latch_unsignaled=1 \
+    qemu.hw.mainkeys=0 \
+    ro.opengles.version=196610 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    ro.zygote.preload.enable=0 \
+    sys.ipo.disable=1 \
     video.accelerate.hw=1
 
 # IMS
@@ -43,24 +41,24 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # OMX: Rank OMX SW codecs lower than OMX HW codecs
 # TODO: Move to vendor_prop.mk once we build vendor image
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0
 
 # Perf
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.kernel.zio=38,108,105,16 \
     ro.mtk_perf_simple_start_win=1 \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
 
 # RIL
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     vendor.rild.libpath=mtk-ril.so \
     vendor.rild.libargs=-d /dev/ttyC0
 
 # WLAN
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     wifi.interface=wlan0 \
     ro.mediatek.wlan.wsc=1 \
     ro.mediatek.wlan.p2p=1 \
